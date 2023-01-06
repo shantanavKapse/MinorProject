359be80610cb777188916df9daa5d857c8208bd8
Shantanav Kapse <shantanavkapse73@gmail.com>
2023-01-06 23:10:18 +0530

    authentication complete

--- app.py
+++ app.py
@@ -1,31 +1,34 @@
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 # Database configuration code.
 app = Flask(__name__)
+app.secret_key = b'Nan36Nut37Sha50Van65'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Nominator.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-import views, models
 
-login_manager = LoginManager()
+login_manager = LoginManager(app=app)
 login_manager.login_view = 'auth.login_candidate'
 login_manager.login_view = 'auth.login_company'
 login_manager.init_app(app)
 
+from models import Candidate, Company
 
-from models import User
 
 @login_manager.user_loader
 def load_user(username):
-    return User.query.get(username)
+    user = Company.query.filter_by(username=username).first()
+    if not user:
+        user = Candidate.query.filter_by(username=username).first()
+    return user
+
 
 from auth import auth as auth_blueprint
-app.register_blueprint(auth_blueprint)
 
+app.register_blueprint(auth_blueprint)
 
 if __name__ == '__main__':
     app.debug = True
