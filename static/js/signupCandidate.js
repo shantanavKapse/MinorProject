63e1702316684cb7de864640750f8f5dd36d60eb
Vanshika432 <vanshikarathore202@gmail.com>
2023-02-16 08:57:46 +0530

    updated password validation

--- static/js/signupCandidate.js
+++ static/js/signupCandidate.js
@@ -1,85 +1,62 @@
 function showpasswd() {
-    let x = document.getElementById("passwd");
-    if (x.type === "password") {
-        x.type = "text";
-    } else {
-        x.type = "password";
-    }
-    }
-
-function chngPage1(){
-    document.querySelector('.page1').style.display = "none";
-    document.querySelector('.page2').style.display = "block";
+  let x = document.getElementById("passwd");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
 }
 
-function backPage2(){
-    document.querySelector('.page1').style.display = "block";
-    document.querySelector('.page2').style.display = "none";
+function chngPage1() {
+  document.querySelector(".page1").style.display = "none";
+  document.querySelector(".page2").style.display = "block";
 }
 
-function chngPage2(){
-    document.querySelector('.page2').style.display = "none";
-    document.querySelector('.page3').style.display = "block";
+function backPage2() {
+  document.querySelector(".page1").style.display = "block";
+  document.querySelector(".page2").style.display = "none";
 }
 
-function backPage3(){
-    document.querySelector('.page3').style.display = "none";
-    document.querySelector('.page2').style.display = "block";
+function chngPage2() {
+  document.querySelector(".page2").style.display = "none";
+  document.querySelector(".page3").style.display = "block";
+}
+
+function backPage3() {
+  document.querySelector(".page3").style.display = "none";
+  document.querySelector(".page2").style.display = "block";
 }
 
 const myInput = document.getElementById("passwd");
-const reqButton = document.getElementById("pass-req-button");
-const msgBox = document.getElementById("message");
+const infoIcon = document.querySelector(".info-icon");
+const msgReq = document.querySelector(".msg-req");
 const letter = document.getElementById("letter");
 const capital = document.getElementById("capital");
 const number = document.getElementById("number");
 const length = document.getElementById("length");
+const special = document.getElementById("specialChar");
 
-myInput.onfocus = function() {
-    if(length.classList.contains("valid") && letter.classList.contains("valid") && capital.classList.contains("valid") && number.classList.contains("valid")){
-        reqButton.style.display = "none";
-    }
-    else{
-        reqButton.style.display = "block";
-        reqButton.style.borderColor = "red"
-    }
-}
+const reg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,15})/;
+myInput.onkeyup = function () {
+  if (myInput.value.match(reg)) {
+    msgReq.style.display = "none";
+    infoIcon.style.display = "none";
+  } else {
+    msgReq.style.display = "block";
+    infoIcon.style.display = "block";
+  }
 
-reqButton.onclick = function(){
-    if(msgBox.style.display == "none"){
-        msgBox.style.display = "block"  
-    }else{
-        msgBox.style.display = "none"
-    }
-}
-
-// myInput.onblur = function() {
-//   msgBox.style.display = "none";
-//   reqButton.style.display = "none";
-// }
-
-myInput.onkeyup = function() {
   const lowerCaseLetters = /[a-z]/g;
-
-  if(length.classList.contains("valid") && letter.classList.contains("valid") && capital.classList.contains("valid") && number.classList.contains("valid")){
-    reqButton.style.display = "none";
-  }
-  else{
-    reqButton.style.display = "block";
-  }
-
-  
-  if(myInput.value.match(lowerCaseLetters)) {  
+  if (myInput.value.match(lowerCaseLetters)) {
     letter.classList.remove("invalid");
     letter.classList.add("valid");
   } else {
     letter.classList.remove("valid");
     letter.classList.add("invalid");
   }
-  
-  // Validate capital letters
+
   const upperCaseLetters = /[A-Z]/g;
-  if(myInput.value.match(upperCaseLetters)) {  
+  if (myInput.value.match(upperCaseLetters)) {
     capital.classList.remove("invalid");
     capital.classList.add("valid");
   } else {
@@ -87,22 +64,29 @@ myInput.onkeyup = function() {
     capital.classList.add("invalid");
   }
 
-  // Validate numbers
+  const specialCharacter = /[!@#\$%\^&\*]/g;
+  if (myInput.value.match(specialCharacter)) {
+    special.classList.remove("invalid");
+    special.classList.add("valid");
+  } else {
+    special.classList.remove("valid");
+    special.classList.add("invalid");
+  }
+
   const numbers = /[0-9]/g;
-  if(myInput.value.match(numbers)) {  
+  if (myInput.value.match(numbers)) {
     number.classList.remove("invalid");
     number.classList.add("valid");
   } else {
     number.classList.remove("valid");
     number.classList.add("invalid");
   }
-  
-  // Validate length
-  if(myInput.value.length >= 8) {
+
+  if (myInput.value.length >= 8 && myInput.value.length <=15) {
     length.classList.remove("invalid");
     length.classList.add("valid");
   } else {
     length.classList.remove("valid");
     length.classList.add("invalid");
   }
-}
+};
